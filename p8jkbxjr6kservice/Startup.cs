a25d2ea25c2630d786ea004acd5c77c7a1acd132
Gilang <sciters@gmail.com>
2016-08-16 18:37:05 +0700

    Add project files.

---
+++ p8jkbxjr6kservice/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(p8jkbxjr6kservice.Startup))]
+
+namespace p8jkbxjr6kservice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
